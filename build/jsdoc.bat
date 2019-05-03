f2d02f433d170d281bc6a785dfcda25c5fca08d8
Katsuomi Kobayashi <katsuomi.kobayashi@gmail.com>
2019-05-03 21:23:43 +0900

    Setup Parcel

--- build/jsdoc.bat
+++
@@ -1,6 +0,0 @@
-set JSDOC=E:\Commands\jsdoc-toolkit
-set TEMPLATE=%JSDOC%\templates\jsdoc
-
-set SRC_DIR=../gamelib/
-
-java -Djsdoc.dir=%JSDOC% -Djsdoc.template.dir=%TEMPLATE% -jar %JSDOC%\jsrun.jar %JSDOC%\app\run.js -d=jsdoc -r=4 %SRC_DIR%
